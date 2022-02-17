289fce7874caa20e80b1c7d158237b70deed6691
Piotr Hołub <piotrekholub@wp.pl>
2022-02-17 16:44:57 +0100

    Added validation to Login component

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import "./login.scss";
 import logo from "../assets/logo.png";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="login">
@@ -12,19 +24,59 @@ export const Login = () => {
           <img className="login__logo-img" src={logo} alt="logo" />
           <h1 className="login__logo-title">Weather App</h1>
         </div>
-        <form className="login__form">
+        <form className="login__form" onSubmit={handleSubmit(onSubmit)}>
           <label className="login__form-label">
             <p>Email:</p>
-            <input className="login__form-input" type="text" />
+            <input
+              {...register("email", {
+                required: "Proszę podać poprawny adres email!",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Podany email jest nieprawidłowy!",
+                },
+              })}
+              className="login__form-input"
+              type="text"
+            />
+            {errors.email && (
+              <p className="login__form-error">{errors.email.message}</p>
+            )}
           </label>
           <label className="login__form-label">
             <p>Hasło:</p>
-            <input className="login__form-input" type="password" />
+            <input
+              {...register("password", {
+                required: "Proszę podać hasło!",
+                minLength: {
+                  value: 8,
+                  message: "Podane hasło jest za krótkie!",
+                },
+              })}
+              className="login__form-input"
+              type="password"
+            />
+            {errors.password && (
+              <p className="login__form-error">{errors.password.message}</p>
+            )}
           </label>
           {isSignUp && (
             <label className="login__form-label">
               <p>Powtórz hasło:</p>
-              <input className="login__form-input" type="password" />
+              <input
+                {...register("repeatedPassword", {
+                  required: "Ponownie wporwadź hasło!",
+                  validate: (value) =>
+                    value === watch("password") ||
+                    "Podane hasła nie są takie same!",
+                })}
+                className="login__form-input"
+                type="password"
+              />
+              {errors.repeatedPassword && (
+                <p className="login__form-error">
+                  {errors.repeatedPassword.message}
+                </p>
+              )}
             </label>
           )}
           <div className="login__form-buttons">
@@ -32,8 +84,12 @@ export const Login = () => {
               {isSignUp ? "Utwórz konto" : "Zaloguj się"}
             </button>
             <button
+              type="button"
               className="login__form-btn"
-              onClick={() => setIsSignUp((prev) => !prev)}
+              onClick={() => {
+                reset();
+                setIsSignUp((prev) => !prev);
+              }}
             >
               {isSignUp ? "Wróć" : "Załóż konto"}
             </button>
