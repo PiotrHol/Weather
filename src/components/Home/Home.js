44742273e5b98124fac1d433460226da444ce069
Piotr Hołub <piotrekholub@wp.pl>
2022-02-20 12:15:54 +0100

    Created Home component and added HomHeader component to it

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.scss";
+import { HomeHeader } from "../HomeHeader/HomeHeader";
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <HomeHeader />
+        </div>
+    );
+}
