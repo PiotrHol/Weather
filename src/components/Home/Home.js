e308e77ae4e9e7476d9d84d8511f0de77ab843f8
Piotr Hołub <piotrekholub@wp.pl>
2022-02-21 20:06:10 +0100

    Added Weather component to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./home.scss";
 import { HomeHeader } from "../HomeHeader/HomeHeader";
+import { useSelector } from "react-redux";
+import { Weather } from "../Weather/Weather";
 
 export const Home = () => {
+    const cities = useSelector(state => state.cities.selected);
+
     return (
         <div className="home">
             <HomeHeader />
+            <div className="home__weather">
+                {cities.map(city => <Weather key={city.id} id={city.id} name={city.name} />)}
+            </div>
         </div>
     );
 }
