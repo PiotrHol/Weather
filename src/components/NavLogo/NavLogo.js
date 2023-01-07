86a1d699a0f49f8134a9b6dff76e4b57f666745d
Piotr Hołub <piotrekholub@wp.pl>
2023-01-07 20:01:15 +0100

    Added new state for Navigation component

--- src/components/NavLogo/NavLogo.js
+++ src/components/NavLogo/NavLogo.js
@@ -3,10 +3,14 @@ import "./navLogo.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
 
-export const NavLogo = () => {
+export const NavLogo = ({ setIsShowMenu }) => {
   return (
     <div className="nav-logo">
-      <FontAwesomeIcon icon={faCloudSun} className="nav-logo__icon" />
+      <FontAwesomeIcon
+        icon={faCloudSun}
+        className="nav-logo__icon"
+        onClick={setIsShowMenu}
+      />
     </div>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navigation.scss";
 import { NavLogo } from "../NavLogo/NavLogo";
 
 export const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="navigation">
-      <NavLogo />
+      <NavLogo setIsShowMenu={() => setShowMenu((prev) => !prev)} />
     </div>
   );
 };
