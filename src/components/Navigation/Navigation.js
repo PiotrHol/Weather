23fa7a0324228481a86b3263142838d0457de1c2
Piotr Hołub <piotrekholub@wp.pl>
2023-01-07 19:38:05 +0100

    Created Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navigation.scss";
+import { NavLogo } from "../NavLogo/NavLogo";
+
+export const Navigation = () => {
+  return (
+    <div className="navigation">
+      <NavLogo />
+    </div>
+  );
+};
