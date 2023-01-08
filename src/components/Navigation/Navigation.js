ba7d5d6591c6a3e06f302f97d420181572b5f932
Piotr Hołub <piotrekholub@wp.pl>
2023-01-08 20:45:54 +0100

    Added NavMenu to Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./navigation.scss";
 import { NavLogo } from "../NavLogo/NavLogo";
+import { NavMenu } from "../NavMenu/NavMenu";
 
 export const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -8,6 +9,7 @@ export const Navigation = () => {
   return (
     <div className="navigation">
       <NavLogo setIsShowMenu={() => setShowMenu((prev) => !prev)} />
+      <NavMenu className="navigation__menu" isShowMenu={showMenu} />
     </div>
   );
 };
