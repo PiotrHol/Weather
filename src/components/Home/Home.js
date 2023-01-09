2323369fde98b4d55f50968ee42444644daf0c74
Piotr Hołub <piotrekholub@wp.pl>
2023-01-09 17:39:56 +0100

    Added Navigation component and deleted HomeHeader component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./home.scss";
-import { HomeHeader } from "../HomeHeader/HomeHeader";
+import { Navigation } from "../Navigation/Navigation";
 import { useSelector } from "react-redux";
 import { Weather } from "../Weather/Weather";
 
 export const Home = () => {
-    const cities = useSelector(state => state.cities.selected);
+  const cities = useSelector((state) => state.cities.selected);
 
-    return (
-        <div className="home">
-            <HomeHeader />
-            <div className="home__weather">
-                {cities.map(city => <Weather key={city.id} id={city.id} name={city.name} />)}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="home">
+      <Navigation />
+      <div className="home__weather">
+        {cities.map((city) => (
+          <Weather key={city.id} id={city.id} name={city.name} />
+        ))}
+      </div>
+    </div>
+  );
+};
