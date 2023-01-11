938e96100c6b1841340f96ba87c5be4e7aad9326
Piotr Hołub <piotrekholub@wp.pl>
2023-01-11 18:35:20 +0100

    Updated NavMenu component

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./navMenu.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { useHistory } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { removeUser } from "../../redux/actions/authActions";
 import classNames from "classnames";
 
-export const NavMenu = ({ isShowMenu }) => {
-  const history = useHistory();
+export const NavMenu = ({ isShowMenu, setIsShowMenu, setIsShowSettings }) => {
   const dispatch = useDispatch();
+
+  const handleOpenSettings = () => {
+    setIsShowMenu(false);
+    setIsShowSettings(true);
+  };
 
   const handleLogOut = async () => {
     await signOut(getAuth());
@@ -23,7 +26,7 @@ export const NavMenu = ({ isShowMenu }) => {
         icon={faGear}
         className="nav-menu__icon"
         title="Setting"
-        onClick={() => history.push("/setting")}
+        onClick={handleOpenSettings}
       />
       <FontAwesomeIcon
         icon={faRightFromBracket}
