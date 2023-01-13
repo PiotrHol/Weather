38305b937d98a6f23f85713930debd0c1800b766
Piotr Hołub <piotrekholub@wp.pl>
2023-01-13 18:32:42 +0100

    Updated structure of City component

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -1,22 +1,4 @@
 import React from "react";
 import "./city.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteCity } from "../../redux/actions/cityActions";
-import { getFirestore, doc, updateDoc, arrayRemove } from "firebase/firestore";
 
-export const City = ({ city }) => {
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth.id);
-
-  const handleClick = async () => {
-    dispatch(deleteCity(city.name));
-    await updateDoc(doc(getFirestore(), "users", auth), {
-      cities: arrayRemove(city),
-    });
-  };
-  return (
-    <div className="city" onClick={handleClick}>
-      {city.name}
-    </div>
-  );
-};
+export const City = ({ city }) => <div className="city">{city.name}</div>;
